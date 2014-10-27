597698d1387e5124d3a1ca6de7043b392d947fa8
unknown <kiiroifuriku@hotmail.com>
2014-10-27 21:48:19 +0800

    Add blinking text for readability

--- js/script.js
+++ js/script.js
@@ -1,38 +1,66 @@
 (function($) {
-    $(function() {
-        function makeBg(sel) {
-            $(sel).text('');
+    var blinkState = {};
 
-            function makeQuadNode(depth) {
-                var max = 2 + Math.floor(Math.random() * 3);
-                if(depth >= max)
-                    return '<div class="imgbox" style="background-image: url(\'img/1411207249968.gif\'); opacity: ' + Math.random() / 2 + '"></div>';
-                if(Number(new Date()) % 4 == 0 && depth > 1)
-                    return '<div class="imgbox" style="background-image: url(\'img/1411207249968.gif\'); opacity: ' + Math.random() / 2 + '"></div>';
+    function makeBg(sel) {
+        $(sel)
+            .text('')
+            .css({
+                overflow: 'hidden'
+            });
 
-                return [
-                    '<ul class="quad-wrapper">',
-                    '<li>' + makeQuadNode(depth + 1) + '</li>' +
-                    '<li>' + makeQuadNode(depth + 1) + '</li>' +
-                    '<li>' + makeQuadNode(depth + 1) + '</li>' +
-                    '<li>' + makeQuadNode(depth + 1) + '</li>' +
-                    '</ul>'
-                ].join('');
-            };
+        function makeQuadNode(depth) {
+            var max = 2 + Math.floor(Math.random() * 3);
+            if(depth >= max)
+                return '<div class="imgbox" style="background-image: url(\'img/1411207249968.gif\'); opacity: ' + (0.35 + Math.random() / 2) + '"></div>';
+            if(Number(new Date()) % 4 == 0 && depth > 1)
+                return '<div class="imgbox" style="background-image: url(\'img/1411207249968.gif\'); opacity: ' + (0.35 + Math.random() / 2) + '"></div>';
 
-            var mainQuad = $('<ul>');
+            return [
+                '<ul class="quad-wrapper">',
+                '<li>' + makeQuadNode(depth + 1) + '</li>' +
+                '<li>' + makeQuadNode(depth + 1) + '</li>' +
+                '<li>' + makeQuadNode(depth + 1) + '</li>' +
+                '<li>' + makeQuadNode(depth + 1) + '</li>' +
+                '</ul>'
+            ].join('');
+        };
 
-            for(var i = 1, depth = 0; i <= 4; i++) {
-                $('<li>')
-                    .html(makeQuadNode(depth))
-                    .appendTo(mainQuad);
-            }
+        var mainQuad = $('<ul>');
 
-            $(mainQuad)
-                .addClass('quad-wrapper')
-                .appendTo(sel);
+        for(var i = 1, depth = 0; i <= 4; i++) {
+            $('<li>')
+                .html(makeQuadNode(depth))
+                .appendTo(mainQuad);
         }
 
+        $(mainQuad)
+            .addClass('quad-wrapper')
+            .css({
+                position: 'absolute',
+                width: 1600,
+                height: 1200,
+                left: ($(sel).width() - 1600) / 2,
+                top: ($(sel).height() - 1200) / 2
+            })
+            .appendTo(sel);
+    }
+
+    function blinkSel(sel, delay) {
+        if(!blinkState[sel])
+            blinkState[sel] = true;
+
+        window.setInterval(function() {
+            $(sel).css({ 'visibility': blinkState[sel] ? 'visible' : 'hidden' });
+            blinkState[sel] = !blinkState[sel];
+        }, delay);
+    }
+
+    $(function() {
         makeBg("#homu");
+        blinkSel("#main-content > *", 250);
     });
+
+    $(window).on('resize', function() {
+        makeBg("#homu");
+    })
 })(window.jQuery);
