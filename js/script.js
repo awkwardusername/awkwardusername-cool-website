f49d4d90a7ba9360c5be58a7e13b262e2eedf7b6
unknown <kiiroifuriku@hotmail.com>
2014-10-27 20:39:34 +0800

    Consider design requirements for client + other things.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-$(document).ready(function() {
-alert(1)
+(function($) {
+    $(function() {
+        function makeQuadNode() {
+            if (Number(new Date()) % 4 != 0)
+                return '<div class="imgbox" style="background-image: url(\'img/1411207249968.gif\')"></div>';
+            return '<ul class="quad-wrapper"><li>' + makeQuadNode() + '</li><li>' + makeQuadNode() + '</li><li>' + makeQuadNode() + '</li><li>' + makeQuadNode() + '</li></ul>';
+        };
 
-  $(h1).on("mouseenter", function() {
-  
-  });
-});
+        var mainQuad = $('<ul>');
+
+        for(var i = 1; i <= 4; i++)
+            $('<li>')
+                .html(makeQuadNode())
+                .appendTo(mainQuad);
+
+        $(mainQuad)
+            .addClass('quad-wrapper')
+            .appendTo('#homu');
+    });
+})(window.jQuery);
